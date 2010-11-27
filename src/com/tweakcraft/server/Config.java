80e8c06b7eef46ad6db1716f0a135656cbfa413c
vistu <sjoerd.huininga@gmail.com>
2010-11-27 15:21:52 +0100

    Username validation added

--- src/com/tweakcraft/server/Config.java
+++ src/com/tweakcraft/server/Config.java
@@ -7,4 +7,6 @@ package com.tweakcraft.server;
 public class Config {
 
     public static int PORT = 25565;
+    public static String SERVER_HASH = "2153bf8cf147a2bcec9fe761f6e98e66";
+    public static String LOGIN_HANDLER = "http://www.minecraft.net/game/checkserver.jsp?";
 }
