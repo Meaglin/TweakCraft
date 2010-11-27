2d86ccc8fd3b0cfbcf8145cc6bb396bcc1197f9d
Meaglin <meaglin.wasabi@gmail.com>
2010-11-27 18:27:38 +0100

    Cleaned up onLoginRequest, add offline Mode parameter.

--- src/com/tweakcraft/server/instance/Player.java
+++ src/com/tweakcraft/server/instance/Player.java
@@ -5,12 +5,8 @@ import com.tweakcraft.server.network.BaseSendablePacket;
 import com.tweakcraft.server.network.GameClient;
 import com.tweakcraft.server.network.sendablePacket.*;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
 /**
@@ -24,38 +20,62 @@ public class Player extends Character{
 	_client = client;
     }
 
-    public void onLoginRequest(int protocol, String username, String password, Long mapSeed, byte dimension) throws MalformedURLException, IOException{
+    public void onLoginRequest(int protocol, String username, String password, Long mapSeed, byte dimension){
 	System.out.println("Login req prot"+protocol+"u:"+username+" p "+password+" mS "+mapSeed );
-        //Okay, user wants to login, we'll have to check whether the user is who claims he is
-        String urlStr = Config.LOGIN_HANDLER+"user=" + username + "&serverId="+Config.SERVER_HASH; //+global_hash;
-        //System.out.println("Server req:"+ urlStr);
-        URL url = new URL(urlStr);
-        URLConnection conn = url.openConnection ();
-        // Get the response
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuffer sb = new StringBuffer();
-        String serverResponse;
-        serverResponse = rd.readLine();
-        //System.out.println("Server response:"+ serverResponse+":");
-        if(serverResponse.equals("YES")){
-            sendPacket(new ErrorMessage("Not supported yet."));
-        }else{
-            sendPacket(new ErrorMessage("Failed to login: User not premium"));
-        }
-    }
-    // not implemented yet.
-    public void onLogin(){
 
+	if(!Config.OFFLINE_MODE && !validUser(username))
+	    sendPacket(new ErrorMessage("Failed to login: User not premium"));
+	else
+	    sendPacket(new ErrorMessage("Not supported yet."));
     }
+
+    private boolean validUser(String username) {
+	//Okay, user wants to login, we'll have to check whether the user is who claims he is
+	String urlStr = Config.LOGIN_HANDLER + "user=" + username + "&serverId=" + Config.SERVER_HASH; //+global_hash;
+	//System.out.println("Server req:"+ urlStr);
+	String serverResponse = "";
+	BufferedReader rd = null;
+	InputStreamReader str = null;
+	try{
+	    URL url = new URL(urlStr);
+	    URLConnection conn = url.openConnection();
+
+	    str = new InputStreamReader(conn.getInputStream());
+	    rd = new BufferedReader(str);
+
+	    // Get the response
+	    serverResponse = rd.readLine();
+	}catch(Exception e){
+	    e.printStackTrace();
+	}finally {
+	    try{
+		if(str != null)str.close();
+		if(rd != null) rd.close();
+	    }catch(Exception e){}
+	}
+	if (serverResponse.equals("YES"))
+	    return true;
+	else
+	    return false;
+    }
+
+    // not implemented yet.
+    public void onLogin() {
+    }
+
     public void onHandShake(String message){
 	sendPacket(new SendHandshake());
     }
+
     public void onDisconnect(){
 	
     }
+
     public GameClient getClient(){
 	return _client;
     }
+
+
     public void sendPacket(BaseSendablePacket packet){
 	getClient().sendPacket(packet);
     }
--- src/com/tweakcraft/server/network/sendablePacket/SendHandshake.java
+++ src/com/tweakcraft/server/network/sendablePacket/SendHandshake.java
@@ -13,8 +13,13 @@ public class SendHandshake extends BaseSendablePacket {
     @Override
     protected void writeImpl() {
 	writeByte(0x02);
-	writeString(Config.SERVER_HASH);
-        _log.log(Level.INFO, "Serverh hash:{0}", Config.SERVER_HASH);
+	
+	if(Config.OFFLINE_MODE)
+	    writeString("-");
+	else
+	    writeString(Config.SERVER_HASH);
+
+	//_log.log(Level.INFO, "Serverh hash:{0}", Config.SERVER_HASH);
         //Moet netter; dit is de 'unieke' serverhash, dient eigenlijk per server gemaakt te worden
         //deze hash is nodig om name verification te ondersteunen
         //Liefst dus eenmalig aanmaken en opslaan, dan ergens uit laden
